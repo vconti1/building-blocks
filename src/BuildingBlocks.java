01c049f37a108623a48e033693f016ab336c2855
vconti1 <vincentconti393@gmail.com>
2024-11-09 23:11:34 -0500

    Add floor graphic

--- src/BuildingBlocks.java
+++ src/BuildingBlocks.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,17 +16,17 @@ public class BuildingBlocks extends JPanel implements ActionListener, KeyListene
 	final int height = 16;
 	final int boardWidth = 360;
 	final int boardHeight = 640;
-	int blockX = boardWidth/8;
+	int blockX = boardWidth/2;
 	int blockY = boardHeight/2;
-	int blockWidth = 34;
-	int blockHeight = 24;
+	int blockWidth = 32;
+	int blockHeight = 32;
+	int floorOffset = 125;
 	
 	class Block{
 		int x = blockX;
 		int y = blockY;
 		int width = blockWidth;
 		int height = blockHeight;
-		
 		
 		
 		Block(){
@@ -38,15 +39,28 @@ public class BuildingBlocks extends JPanel implements ActionListener, KeyListene
 		setFocusable(true);
 		addKeyListener(this);
 	}
-	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		draw(g);
+	}
 	public void draw(Graphics g) {
-		g.drawRect(blockX, blockY, blockWidth, blockHeight);
+		
+		Graphics2D block = (Graphics2D) g;
+		Graphics2D floor = (Graphics2D) g;
+		
+		floor.setPaint(Color.white);
+		floor.fillRect(0, boardHeight-floorOffset, boardWidth, blockX/8);
+		
+		//block.setPaint(Color.white);
+		//block.fillRect(blockX, blockY, blockWidth, blockHeight);
+		
+		//g.drawRect(blockX, blockY, blockWidth, blockHeight);
+		
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
